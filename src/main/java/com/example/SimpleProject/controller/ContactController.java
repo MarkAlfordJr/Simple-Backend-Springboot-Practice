b3cd33717e48d5774580700b024ba8981195ab67
Mark Alford Jr <mark.alford0424@gmail.com>
2026-07-03 15:39:13 -0400

    project completed with simple CRUD operations

--- src/main/java/com/example/SimpleProject/controller/ContactController.java
+++ src/main/java/com/example/SimpleProject/controller/ContactController.java
@@ -32,9 +32,29 @@ public class ContactController {
     }
 
     // POST METHOD, use @response body, for parameters
+    /*
+        this post request is only available in POSTMAN.
+        1. connect folder or repo to post
+        2. make a new request in PM
+        3. select type
+        4. put 'localhost:8090/api/contact/post' in field
+        5. go to body tab, make sure raw and JSON are present
+        6. fill out JSON according to Model structure
+     */
     @PostMapping("/post")
     public ResponseEntity<ApiResponse<Contact>> createContacts(@RequestBody Contact contact){
         return ResponseEntity.ok(contactService.addContact(contact));
+    }
+
+    @DeleteMapping("/delete/{id}") // the {id} is a param that comes from @PathVariable
+    public ResponseEntity<ApiResponse<?>> delete(@PathVariable int id){
+        return ResponseEntity.ok(contactService.deleteContact(id));
+    }
+
+    // updating, PUT api
+    @PutMapping("/update/{id}")
+    public ResponseEntity<ApiResponse<Contact>> updateContact(@PathVariable int id, @RequestBody Contact contact) {
+        return ResponseEntity.ok(contactService.updateContact(id, contact));
     }
 
 }
